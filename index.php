ddbf0eddb0d7926e18e5dc0c9312373b3062ab34
Topy <Clement.orlandini.etu@univ-lemans.fr>
2019-02-07 11:00:27 +0100

    Refonte CSS 1

--- index.php
+++ index.php
@@ -1,16 +1,14 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8" />
-        <title>ONGAKU Le jeu musical !</title>
-        <meta name="description" content=" Jouez à Ongaku ! Retrouvez des paroles de musiques selon votre sélection ! Trouvez les paroles dans le temps imparti, et devenez le meilleur de tous vos amis !" />
+        <title>ONGAKU ! - Le jeu musical</title>
+        <meta name="description" content=" Jouez à Ongaku ! Retrouvez des paroles de musiques selon votre selection ! Trouvez les paroles dans le temps imparti, et devenez le meilleur de tous vos amis !" />
 
-        <link rel="stylesheet" href="css/styleGeneral.css" type="text/css" />
-        <link rel="stylesheet" href="css/index.css" type="text/css" />
-        <link rel="stylesheet" type="text/css" href="css/header.css" />
+        <link rel="stylesheet" href="./styles/css/styles.css" />
         <link rel="shortcut icon" href="medias/images/boutonVert.png">
     </head>
 
-    <body>
+    <body id="index">
         <?php include("header.php"); ?>
 
         <main>
